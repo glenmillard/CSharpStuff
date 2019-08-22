6a3aa7065441424ce2a058d5d9e0082b5f9bf92f
Glen Millard <glenmillard@gmail.com>
2019-08-22 08:06:36 -0700

    This is my project!

---
+++ IODemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace IODemo //reads a directory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            System.IO.Directory.CreateDirectory(@"c:\Allfiles\glen");
+            System.IO.DirectoryInfo dirinfo = new System.IO.DirectoryInfo(@"c:\Allfiles");
+            foreach(var subdir in dirinfo.GetDirectories())
+            {
+                Console.WriteLine(subdir.Name);
+                Console.WriteLine(subdir.CreationTime); //shows creation time
+                Console.WriteLine(subdir.Attributes);
+                Console.WriteLine(subdir.CreationTimeUtc);
+            }
+            Console.ReadLine();
+        }
+    }
+}
